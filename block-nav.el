8bbe9eb09a765c81fe386675d23a622772058592
Philip Dumaresq <phdumaresq@protonmail.com>
2020-09-26 15:54:27 -0400

    Added navigating to next and previous indentation levels

--- block-nav.el
+++ block-nav.el
@@ -3,16 +3,31 @@
   (forward-line dir)
   (back-to-indentation)
   (cond ((< original-column (current-column))
-         (space-jump-move-block dir original-column))
+         (block-nav-move-block dir original-column))
         ((string-empty-p (current-line-contents))
-         (space-jump-move-block dir original-column))))
+         (block-nav-move-block dir original-column))))
 
 (defun block-nav-next-block ()
   (interactive)
-  (space-jump-move-block 1 (current-column)))
+  (block-nav-move-block 1 (current-column)))
 
 (defun block-nav-previous-block ()
   (interactive)
-  (space-jump-move-block -1 (current-column)))
+  (block-nav-move-block -1 (current-column)))
+
+(defun block-nav-move-indentation-level (dir original-column)
+  (interactive)
+  (forward-line dir)
+  (back-to-indentation)
+  (cond ((= original-column (current-column))
+         (block-nav-move-indentation-level dir original-column)))) 
+  
+(defun block-nav-next-indentation-level ()
+  (interactive)
+  (block-nav-move-indentation-level 1 (current-column)))
+
+(defun block-nav-previous-indentation-level ()
+  (interactive)
+  (block-nav-move-indentation-level -1 (current-column)))
 
 (provide 'block-nav)
