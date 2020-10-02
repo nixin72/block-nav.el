0a8e88d21ffa5f20403d9370ce9a3cf58355d049
Philip Dumaresq <phdumaresq@protonmail.com>
2020-10-01 23:53:24 -0400

    Made lots of changes as suggested by linting tools to get this ready for melpa

--- block-nav.el
+++ block-nav.el
@@ -1,11 +1,11 @@
-;;; block-nav.el --- Block level navigation for quick jumping -*- lexical-binding: t; -*-
+;;; block-nav.el --- Jump across indentation levels for quick navigation -*- lexical-binding: t; -*-
 ;;
 ;; Copyright © 2020 Philip Dumaresq
 ;;
 ;; Authors: Philip Dumaresq <phdumaresq@protonmail.com>
 ;; Maintainer: Philip Dumaresq <phdumaresq@protonmail.com>
 ;; URL: http://github.com/nixin72/block-nav.el
-;; Keywords: navigation
+;; Keywords: convenience
 ;; Version: 0.1.0
 ;; Package-Requires: ((emacs "25.1"))
 ;;
@@ -29,7 +29,7 @@
 ;; Provides a number of interactive functions for easily navigating
 ;; between blocks of code at the same level of indentation.
 ;;
-;; This package does not bind any keys for you. 
+;; This package does not bind any keys for you.
 ;; Here are some example bindings for evil-mode:
 ;;
 ;;   (define-key evil-motion-state-map "H" 'block-nav-previous-indentation-level)
@@ -42,25 +42,23 @@
 ;;; Code:
 
 (defvar block-nav-center-after-scroll nil
-  "When not-nil, Emacs will recenter the current line after moving")
+  "When not-nil, Emacs will recenter the current line after moving.")
 (defvar block-nav-move-skip-shallower t
-  "
-  When not-nil, calling `block-nav-next/previous-block` will 
-  skip over lines with a shallower indentation than the current line.
-  ")
+  "When not-nil, calling `block-nav-next/previous-block` will skip over lines with a shallower indentation than the current line.")
 
-(defmacro do-while (cond &rest body)
-  "Runs the body once, then runs it again in a while loop with the condition."
+;;; Helper functions
+
+(defmacro block-nav-do-while (cond &rest body)
+  "Run the BODY once, then run it again in a while loop with the COND."
   `(progn
      (progn . ,body)
      (while ,cond
        (progn . ,body))))
 
-(defun test-end-of-space (dir)
-  "
-  Returns true if the current line is either the first line in the file
-  or if the current line is the last line in the file or beyond the last line
-  "
+(defun block-nav-test-end-of-space (dir)
+  "Return non-nil if either of the following are true:
+DIR is greater than 0 and you're at the last line of the file.
+DIR is less than 0 and you're at the first line of the file."
   (or
    (and (> dir 0)
         (<= (count-lines (point-min) (point-max))
@@ -68,107 +66,84 @@
    (and (< dir 0)
         (= 1 (line-number-at-pos)))))
 
-(defun block-nav-move-block (dir original-column)
-  "
-  Moves the cursor to the beginning of the next line that shares the same level of indentation.
-  When `dir` is positive, move to the next line, when `dir` is negative, move to the previous line.
-  Original column should be the value of `(current-column)` when the function is initially called.
-  "
-  (interactive)
-  (catch 'reached-end-of-file
-   (let ((line-count 0))
-     (do-while (or (if block-nav-move-skip-shallower
-                       (/= original-column (current-column))
-                       (< original-column (current-column)))
-                   (string-empty-p (buffer-substring
-                                    (line-beginning-position)
-                                    (line-end-position))))
-       (when (test-end-of-space dir)
-         (message "Reached last block of this indentation.")
-         (throw 'reached-end-of-file 0))
-       (forward-line dir)
-       (back-to-indentation)
-       (setf line-count (+ 1 line-count)))
-     line-count)))
-
-(defun finish-move (line-count)
-  "
-  Will take in a number of lines to move, then will jump forward/backwards
-  that many lines, and jump to the first non-whitespace character.
-  If block-nav-center-after-scroll is non-nil, then it will also recenter the
-  current line in the middle of the window.
-  "
+(defun block-nav-do-move (line-count)
+  "Given a LINE-COUNT, will move forwards/backwards that many lines.
+It will then place the cursor at the first non-whitespace character.
+If `block-nav-center-after-scroll' is non-nil, it will recenter the current line"
   (forward-line line-count)
   (back-to-indentation)
   (when block-nav-center-after-scroll
     (recenter)))
 
+;;; Core functions
+
+(defun block-nav-move-block (dir)
+  "Move to the next or previous block with the same level of indentation.
+When DIR is positive, move to the next line.
+When DIR is negative, move to the previous line."
+  (save-excursion
+   (catch 'reached-end-of-file
+    (let ((line-count 0)
+          (original-column (current-column)))
+      (block-nav-do-while (or (if block-nav-move-skip-shallower
+                                  (/= original-column (current-column))
+                                  (< original-column (current-column)))
+                              (string-empty-p (buffer-substring
+                                               (line-beginning-position)
+                                               (line-end-position))))
+        (when (test-end-of-space dir)
+          (message "Reached last block of this indentation.")
+          (throw 'reached-end-of-file 0))
+        (forward-line dir)
+        (back-to-indentation)
+        (setf line-count (+ 1 line-count)))
+      line-count))))
+
+(defun block-nav-move-indentation-level (dir)
+  "Move to a block that has a deeper/shallower level of indentation.
+When DIR is positive, move to the next line with deeper indentation.
+When DIR is negative, move to the previous line with shallower indentation."
+  (save-excursion
+   (catch 'reached-end-of-file
+    (let ((line-count 0)
+          (original-column (current-column)))
+      (block-nav-do-while (or
+                           (and (> dir 0)
+                                (>= original-column (current-column)))
+                           (and (< dir 0)
+                                (<= original-column (current-column))))
+        (when (test-end-of-space dir)
+          (if (> dir 0)
+              (message "Deepest indentation reached")
+              (message "Shallowest indentation reached"))
+          (throw 'reached-end-of-file 0))
+        (forward-line dir)
+        (back-to-indentation)
+        (setf line-count (+ dir line-count)))
+      line-count))))
+
+;;; Interactive functions to wrap functionality
+
 (defun block-nav-next-block ()
-  "
-  Calls `block-nav-move-block` with 
-  the arguments necessary to go to the next block.
-  "
+  "Move the cursor to the next line that shares the same level of indentation."
   (interactive)
-  (let ((move-lines
-         (save-excursion
-           (block-nav-move-block 1 (current-column)))))
-    (finish-move move-lines)))
+  (block-nav-do-move (block-nav-move-block 1)))
 
 (defun block-nav-previous-block ()
-  "
-  Calls `block-nav-move-block` with 
-  the arguments necessary to go to the previous block.
-  "
+  "Move the cursor to previous line that shares the same level of indentation."
   (interactive)
-  (let ((move-lines
-         (save-excursion
-           (block-nav-move-block -1 (current-column)))))
-    (finish-move (* move-lines -1))))
-
-(defun block-nav-move-indentation-level (dir original-column)
-  "
-  Moves the cursor to the start of the next line that has a deeper/shallower level of indentation.
-  When `dir` is positive, move to the next line, when `dir` is negative, move to the previous line.
-  Original column should be the value of `(current-column)` when the function is initially called.
-  "
-  (interactive)
-  (catch 'reached-end-of-file
-   (let ((line-count 0))
-     (do-while (or
-                (and (> dir 0)
-                     (>= original-column (current-column)))
-                (and (< dir 0)
-                     (<= original-column (current-column))))
-       (when (test-end-of-space dir)
-         (if (> dir 0)
-             (message "Deepest indentation reached")
-             (message "Shallowest indentation reached"))
-         (throw 'reached-end-of-file 0))
-       (forward-line dir)
-       (back-to-indentation)
-       (setf line-count (+ dir line-count)))
-     line-count)))
+  (block-nav-do-move (* (block-nav-move-block -1) -1)))
 
 (defun block-nav-next-indentation-level ()
-  "
-  Calls `block-nav-move-indentation-level` with 
-  the arguments necessary to go deeper in indentation.
-  "
+  "Move the cursor to the next line that has a deeper level of indentation."
   (interactive)
-  (let ((move-lines
-         (save-excursion
-           (block-nav-move-indentation-level 1 (current-column)))))
-    (finish-move move-lines)))
+  (block-nav-do-move (block-nav-move-indentation-level 1)))
 
 (defun block-nav-previous-indentation-level ()
-  "
-  Calls `block-nav-move-indentation-level` with 
-  the arguments necessary to go back shallower in indentation.
-  "
+  "Move the cursor to the previous line that has shallower level of indentation."
   (interactive)
-  (let ((move-lines
-         (save-excursion
-           (block-nav-move-indentation-level -1 (current-column)))))
-    (finish-move move-lines)))
+  (block-nav-do-move (block-nav-move-indentation-level -1)))
 
 (provide 'block-nav)
+
+;;; block-nav.el ends here
